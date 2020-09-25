ebce8526f2284e5b653f174f9d8f518978ad098f
kh-sonrai-2 <71893749+kh-sonrai-2@users.noreply.github.com>
2020-09-25 13:21:26 -0300

    Update bot.py

--- write_input/bot.py
+++ write_input/bot.py
@@ -2,9 +2,8 @@ import logging
 
 
 def run(ctx):
-    data_enum = ctx.get_policy_evidence_data()
-    logging.info(data_enum)
-    for thing in data_enum:
-        logging.info(thing)
-        logging.info(str(thing))
-        logging.info(type(thing))
+    config = ctx.config
+    # Get custom fields
+    if config:
+        ticket = config.get("data")
+        logging.info(ticket)
