0ec84f325cce1620361986ef2312b66d7275ef51
kh-sonrai-2 <71893749+kh-sonrai-2@users.noreply.github.com>
2020-09-25 13:39:24 -0300

    Update bot.py

--- write_input/bot.py
+++ write_input/bot.py
@@ -3,7 +3,13 @@ import logging
 
 def run(ctx):
     config = ctx.config
+    logging.info("What is in the config?")
+    logging.info(config)
     # Get custom fields
     if config:
         ticket = config.get("data")
+        logging.info("What is in the ticket?")
         logging.info(ticket)
+
+    logging.info("Ticket Evidence:")
+    logging.info(ctx.get_policy_evidence())
